fdbb52b7fba07953cbd1a2ac8d8b5a292581e0de
jonasbarzee <jonascbarzee@gmail.com>
2025-09-18 20:46:21 -0600

    Solved final capture promotion tests for pawn.

--- shared/src/main/java/chess/piecemoves/PawnMovesCalculator.java
+++ shared/src/main/java/chess/piecemoves/PawnMovesCalculator.java
@@ -14,8 +14,7 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
         ChessPiece currentPiece = board.getPiece(position);
         List<ChessMove> validMoves = new ArrayList<>();
 
-
-        // White moves
+         // White moves
         chess.ChessMove forwardOne = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col), null);
         chess.ChessMove forwardTwo = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 2, col), null);
         chess.ChessMove forwardLeft = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col - 1), null);
@@ -25,8 +24,15 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
         chess.ChessMove forwardOnePromoQueen = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col), ChessPiece.PieceType.QUEEN);
         chess.ChessMove forwardOnePromoRook = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col), ChessPiece.PieceType.ROOK);
         chess.ChessMove forwardOnePromoBishop = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col), ChessPiece.PieceType.BISHOP);
-//        chess.ChessMove forwardLeft = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col - 1), null);
-//        chess.ChessMove forwardRight = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col + 1), null);
+        // White promotion capture moves
+        chess.ChessMove forwardLeftPromoKnight = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col - 1), ChessPiece.PieceType.KNIGHT);
+        chess.ChessMove forwardLeftPromoQueen = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col - 1), ChessPiece.PieceType.QUEEN);
+        chess.ChessMove forwardLeftPromoRook = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col - 1), ChessPiece.PieceType.ROOK);
+        chess.ChessMove forwardLeftPromoBishop = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col - 1), ChessPiece.PieceType.BISHOP);
+        chess.ChessMove forwardRightPromoKnight = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col + 1), ChessPiece.PieceType.KNIGHT);
+        chess.ChessMove forwardRightPromoQueen = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col + 1), ChessPiece.PieceType.QUEEN);
+        chess.ChessMove forwardRightPromoRook = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col + 1), ChessPiece.PieceType.ROOK);
+        chess.ChessMove forwardRightPromoBishop = new ChessMove(new ChessPosition(row, col), new ChessPosition(row + 1, col + 1), ChessPiece.PieceType.BISHOP);
 
         // Black moves
         chess.ChessMove backOne = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col), null);
@@ -38,6 +44,14 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
         chess.ChessMove backOnePromoQueen = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col), ChessPiece.PieceType.QUEEN);
         chess.ChessMove backOnePromoRook = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col), ChessPiece.PieceType.ROOK);
         chess.ChessMove backOnePromoBishop = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col), ChessPiece.PieceType.BISHOP);
+        chess.ChessMove backLeftPromoKnight = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col - 1), ChessPiece.PieceType.KNIGHT);
+        chess.ChessMove backLeftPromoQueen = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col - 1), ChessPiece.PieceType.QUEEN);
+        chess.ChessMove backLeftPromoRook = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col - 1), ChessPiece.PieceType.ROOK);
+        chess.ChessMove backLeftPromoBishop = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col - 1), ChessPiece.PieceType.BISHOP);
+        chess.ChessMove backRightPromoKnight = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col + 1), ChessPiece.PieceType.KNIGHT);
+        chess.ChessMove backRightPromoQueen = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col + 1), ChessPiece.PieceType.QUEEN);
+        chess.ChessMove backRightPromoRook = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col + 1), ChessPiece.PieceType.ROOK);
+        chess.ChessMove backRightPromoBishop = new ChessMove(new ChessPosition(row, col), new ChessPosition(row - 1, col + 1), ChessPiece.PieceType.BISHOP);
 
         // White validate moves
         if (currentPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
@@ -51,6 +65,14 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
                 validateMoves(forwardOnePromoQueen, board, validMoves, false);
                 validateMoves(forwardOnePromoRook, board, validMoves, false);
                 validateMoves(forwardOnePromoBishop, board, validMoves, false);
+                validateMoves(forwardLeftPromoKnight, board, validMoves, true);
+                validateMoves(forwardLeftPromoQueen, board, validMoves, true);
+                validateMoves(forwardLeftPromoRook, board, validMoves, true);
+                validateMoves(forwardLeftPromoBishop, board, validMoves, true);
+                validateMoves(forwardRightPromoKnight, board, validMoves, true);
+                validateMoves(forwardRightPromoQueen, board, validMoves, true);
+                validateMoves(forwardRightPromoRook, board, validMoves, true);
+                validateMoves(forwardRightPromoBishop, board, validMoves, true);
 
             } else {
                 validateMoves(forwardOne, board, validMoves, false);
@@ -69,6 +91,14 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
                 validateMoves(backOnePromoQueen, board, validMoves, false);
                 validateMoves(backOnePromoRook, board, validMoves, false);
                 validateMoves(backOnePromoBishop, board, validMoves, false);
+                validateMoves(backLeftPromoKnight, board, validMoves, true);
+                validateMoves(backLeftPromoQueen, board, validMoves, true);
+                validateMoves(backLeftPromoRook, board, validMoves, true);
+                validateMoves(backLeftPromoBishop, board, validMoves, true);
+                validateMoves(backRightPromoKnight, board, validMoves, true);
+                validateMoves(backRightPromoQueen, board, validMoves, true);
+                validateMoves(backRightPromoRook, board, validMoves, true);
+                validateMoves(backRightPromoBishop, board, validMoves, true);
             } else {
                 validateMoves(backOne, board, validMoves, false);
                 validateMoves(backLeft, board,  validMoves, true);
